7bce9896e48651709535e68fbcc8bc928142872f
seanczw <70927991+seanczw@users.noreply.github.com>
2022-10-19 22:41:19 +0800

    updated imports

--- JtProject/src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
@@ -1,6 +1,7 @@
 package com.jtspringproject.JtSpringProject;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
