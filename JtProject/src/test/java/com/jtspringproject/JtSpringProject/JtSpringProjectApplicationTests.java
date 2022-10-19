2fbd8fbe948b3fbb77b7ae177b5477eb2088dcf3
seanczw <70927991+seanczw@users.noreply.github.com>
2022-10-19 23:37:51 +0800

    update JtSpringProjectApplicationTests.java

--- JtProject/src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplicationTests.java
@@ -1,7 +1,7 @@
 package com.jtspringproject.JtSpringProject;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -11,6 +11,10 @@ class JtSpringProjectApplicationTests {
 	void contextLoads() {
 	}
 	
-	
+	@Test
+        @DisplayName("Should be true")
+        void shouldBeTrue() {
+            assertTrue(true);
+        }
 
 }
